30c6c6e889fc91a3ce1c3a83c32017293cb7bdb0
Luiz Pavanello <luizpavanello@gmail.com>
2021-09-23 16:45:19 -0300

    Functions Done

--- exercise_Functions.py
+++ exercise_Functions.py
@@ -46,4 +46,24 @@
 
 # print(len(cadastro_CPF()))
 
-print(dir(__builtins__))
+"""4. Crie uma função Lambda para retornar a soma de 4 valores."""
+
+# sum = lambda num1, num2, num3, num4 : num1 + num2 + num3 + num4
+# print(sum(10,20,30,40))
+
+"""5. Utilize uma função filter para retornar somente os números pares da lista abaixo."""
+
+lista = [5,2,5,7,4,2,6,10,342,54,23,6,7,9,12]
+
+pair = list(filter(lambda x: True if x % 2 == 0 else False,lista))
+
+print(sorted(pair))
+
+"""3. Utilize a função reduce e retorne a soma da lista anterior."""
+
+from functools import reduce
+
+reducer1 = reduce(lambda a, b: a + b, pair)
+
+print(reducer1)
+
