ccc260541c65078fc2ca5c8fe36462c01f85b674
Luiz Pavanello <luizpavanello@gmail.com>
2021-09-25 15:42:54 -0300

    Tuple and Dictionary Exercises

---
+++ Exercises/exercise_Tuple.py
@@ -0,0 +1,23 @@
+"""Exercícios:
+
+1. Crie uma tupla e ordene ela.
+
+tuple1 = (9, 8, 7, 6, 8, 4, 3, 2, 1)
+print(tuple1)
+tuple_lista = list(tuple1)
+tuple_lista.sort()
+print(tuple_lista)
+
+2. Remova todos os valores duplicados da tupla.
+
+3. Crie uma função que busque um determinado valor em uma tupla e retorne a quantidade de vezes que esse valor aparece.
+
+4. Crie dois sets e atribua no primeiro a diferença simétrica entre os dois.
+
+5. Faça a união entre os dois sets e salve o resultado em um terceiro set."""
+
+tuple1 = (9, 8, 7, 6, 8, 4, 3, 2, 1)
+print(tuple1)
+tuple_lista = list(tuple1)
+tuple_lista.sort()
+print(tuple_lista)
