e840642438dcf111c651e93426f8934e2f9d9358
Luiz Pavanello <luizpavanello@gmail.com>
2021-09-27 21:03:53 -0300

    Exercise Case 1,2,3 Update

--- Exercises/all_kinds.py
+++ Exercises/all_kinds.py
@@ -68,3 +68,74 @@ print("Os terrenos tem em média",sum(largura)/len(largura), "M de lagura.")
 print('No total a empresa tem',sum(area),'M, e a média total dos terrenos é de ',sum(area)/len(area),'M2')
 """
 
+"""Case 2 → Você recebeu um arquivo chamado usuarios_feedback, nesse arquivo contém o comentário, nome, gênero e nota de usuários referente a um produto de sua empresas. Você precisa verificar quais as palavras que mais apareceram nos comentários:
+
+
+usuarios_feedback = [{'nome':'Peter','nota':9,'genero':'M','comentario':'bom demais e agil'},
+                {'nome':'Joao','nota':10,'genero':'M','comentario':'agil e eficiente'},
+                {'nome':'user_not_found','nota':0,'genero':'M','comentario':'Horrível'},
+                {'nome':'Marta','nota':10,'genero':'F','comentario':'muito agil bom demais'},
+                {'nome':'Beatriz','nota':10,'genero':'F','comentario':'rápido e eficaz'},
+                {'nome':'user_not_found','nota':2,'genero':'M','comentario':'ruim'},
+                {'nome':'Jéssica','nota':10,'genero':'F','comentario':'agil'},
+                {'nome':'José','nota':7,'genero':'M','comentario':'ok'},
+                {'nome':'Elias','nota':5,'genero':'M','comentario':'precisa melhorar'},
+                {'nome':'Miriã','nota':9,'genero':'F','comentario':'foi muito agil e rápido'},
+                {'nome':'Maria','nota':10,'genero':'F','comentario':'muito bom e agil'}] 
+
+# def frequency(faults):
+#     words = [ ]
+#     for user in faults:
+#         words.extend(user['comentario'].split())
+#         uniq_word = sorted(set(words))
+#     for word in uniq_word:
+#         print(words.count(word), word)
+
+# frequency(usuarios_feedback)
+
+def notas (x):
+  masc = []
+  fem = []
+  for a in x:
+    if a['genero']=='M':
+      masc.append(a['nota'])
+    elif a['genero']=='F':
+      fem.append(a['nota'])
+  print('média masc =',sum(masc)/len(masc),'média fem=',sum(fem)/len(fem))
+  return masc,fem
+masc, fem = notas(usuarios_feedback)
+print(masc,fem)
+
+"""
+"""
+Case 3: Sua empresa te passou uma lista com requisições de treinamentos (muitos repetidos), você tem que definir o cenário que melhor atenda os pedidos, o budget é de 150 moedas e você tem um dicionário que contem o nome do treinamento e o valor dele
+"""
+
+treinamentos = [{'treinamento':'Scrum','moedas':30},
+                     {'treinamento':'Data Science','moedas':40},
+                     {'treinamento':'Gestão de Projetos','moedas':50},
+                     {'treinamento':'Marketing','moedas':30},
+                     {'treinamento':'Cloud','moedas':20},
+                     {'treinamento':'Blockchain','moedas':10},
+                     {'treinamento':'Python','moedas':30}]
+
+pedidos = ['Scrum','Data Science','Gestão de Projetos','Marketing','Cloud','Python','Python','Python',
+           'Scrum','Data Science','Gestão de Projetos','Marketing','Data Science','Gestão de Projetos',
+           'Python','Marketing','Data Science','Gestão de Projetos','Data Science','Gestão de Projetos','Data Science']
+
+def get_value (x,b):
+  for a in x:
+    if a['treinamento']in b:
+      print ('valor:',a['moedas'])
+      return a['moedas']
+
+ped = sorted(set(pedidos))
+for a in ped:
+  print('solicitações:',pedidos.count(a),'\ntreinamento:',a)
+  valor = get_value(treinamentos,a)
+  for b in treinamentos:
+    if a == b['treinamento']:
+     b['total_pedidos']=pedidos.count(a)
+     
+
+
